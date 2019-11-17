fff73b766e35dd1c42866cbf1581f9d141fc1849
smnkrgr <simon.krueger@student.uni-augsburg.de>
2019-11-17 19:51:10 +0100

    Additions in bank!

--- bank.py
+++ bank.py
@@ -5,6 +5,12 @@ from lock import Lock
 
 class Bank:
 
+    starting_position_char = "O"
+    money_position_char = "$"
+    counterboy_char = "C"
+    guard_char = "G"
+    lock_char = "L"
+
     def __init__(self, path):
 
         self.field = self.readField(path)
@@ -20,13 +26,42 @@ class Bank:
 
     def readField(self, path):
 
-        return
+        field = []
+        file = open(path, "r")
+        columns = file.readlines()
+
+        for column in columns:
+            column = column.strip()
+            row = [i for i in column]
+            field.append(row)
+        return field
+    
+    def getPositionsOf(self, char):
+
+        positions = []
+
+        for i in range(len(self.field)):
+            for j in range(len(self.field[0])):
+                if self.field[i][j] == char:
+                    positions.append(i, j)
+
+        return positions
 
     def setStartingPosition(self):
 
-        return
+        positions = self.getPositionsOf(self.starting_position_char)
+        if len(positions) == 0:
+            print("No starting symbol in ascii level. Aborting...")
+            exit()
+        elif len(positions) > 1:
+            print("Multiple starting positions in ascii level. Aborting...")
+            exit()
+        else:
+            return positions[0]
 
     def initializeGuards(self):
+
+        positions = self.getPositionsOf(self.guard_char)
 
         return
 
